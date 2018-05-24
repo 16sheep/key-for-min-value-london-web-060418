2c579a9c8c411f5f342d71efc7bca146c57cc5a4
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-24 06:10:46 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,15 +3,10 @@
 
 def key_for_min_value(name_hash)
   values = name_hash.collect {|key, value| value}
-  print values
-  
   smallest = values[0]
-  print smallest
   for value in values
-  
     if value < smallest
      smallest = value
-     print smallest
     end
   end
   name_hash.key(smallest)
